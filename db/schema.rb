82cee44d4c1019118edbd07a8a39ec1a8cc99384
Екатерина Никифорова <dayso@192.168.1.7>
2023-10-30 21:22:40 +0300

    Adaptives

--- db/schema.rb
+++ db/schema.rb
@@ -66,6 +66,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_18_085534) do
     t.string "first_answer_image"
   end
 
+  create_table "tasks_answers", id: false, force: :cascade do |t|
+    t.integer "task_id"
+    t.integer "answer_id"
+    t.index ["answer_id"], name: "index_tasks_answers_on_answer_id"
+    t.index ["task_id"], name: "index_tasks_answers_on_task_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
