d9c1e949081f06f814b18476dc71c7bdac320234
Uliana <uvvorobyova@edu.hse.ru>
2023-12-03 15:06:05 +0300

    API

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,14 +1,14 @@
 class Api::V1::TasksController < Api::V1::ApplicationController
 
   def index
-    @pins = Task.all
+    @tasks = Task.all
 
     # продвинутый
     # render json: @pins.map { |pin| pin.api_as_json }
   end
 
   def show
-    @pin = Task.find(params[:id])
+    @task = Task.find(params[:id])
   end
 
 end
