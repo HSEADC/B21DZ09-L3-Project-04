63ac31d408a315f7f1c9adc86529737ce2e7b6e5
Kef1r1k <enikiforoff2@yandex.ru>
2023-12-14 12:05:01 +0300

    seeds clean up

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,13 +3,10 @@ raw_text = 'К каждому заданию прилагаются специа
 
 @task_names = [
     "Colour",
-    "Form",
-    "Typeface",
-    "Composition",
-    "Pattern",
-    "Rhythm",
-    "Abstraction",
-    "Sound"
+    "Shape",
+    "Font",
+    "Regularity",
+    "Texture"
 ]
 
 def seed
@@ -70,12 +67,6 @@ def create_sentence(quantity)
   sentence = sentence_words.join(' ').capitalize
 end
 
-def upload_random_avatar
-  uploader = AvatarUploader.new(Answer.new, :avatar)
-  uploader.cache!(File.open(Dir.glob(File.join(Rails.root, 'public/autoupload/images', '*')).sample))
-  uploader
-end
-
 def upload_random_task_image
   uploader = TaskImageUploader.new(Task.new, :first_answer_image)
   uploader.cache!(File.open(Dir.glob(File.join(Rails.root, 'public/autoupload/images', '*')).sample))
@@ -97,12 +88,10 @@ end
 def create_tasks
   @task_names.each do |task_name|
     task = Task.create(
-      name: task_name,
-      description: create_sentence(10..40),
       first_answer_image: upload_random_task_image,
       ref_image: upload_random_ref_image
     )
-    puts "Task with id #{task.id} and name #{task.name} just created"
+    puts "Task with id #{task.id} just created"
   end
 end
 
@@ -114,8 +103,6 @@ def create_answers(quantity)
     user = User.all.sample
     answer = Answer.create(
       task_id: task.id,
-      name: create_sentence(1..3),
-      description: create_sentence(4..20),
       avatar: upload_random_avatar,
       answer_image: upload_random_answer_image,
       user_id: user.id
